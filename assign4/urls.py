e7420f7af2613fd98e75665a0a1f2bd8e58cd599
alexanderdreyer-ndsu <alexander.dreyer@ndsu.edu>
2024-04-03 11:37:36 -0500

    bug fixes

--- assign4/urls.py
+++ assign4/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for assign4 project.
+URL configuration for locallibrary project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -21,20 +21,21 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# Use include() to add paths from the catalog application
 from django.urls import include
 
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
 
+# Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
-
 urlpatterns += [
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
 
+# Use static() to add URL mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
